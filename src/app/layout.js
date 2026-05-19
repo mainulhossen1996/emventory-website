135f546a1f4bc7a1d40f460f0787d7116d3c7f22
Md. Mainul Hossen <123573657+mainulhossen1996@users.noreply.github.com>
2026-05-20 03:23:42 +0600

    font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,22 @@
 import "./globals.css";
 import Nav from "@/components/Navbar/Nav";
+import { Fira_Sans } from "next/font/google";
+
+// Google Font
+const firaSans = Fira_Sans({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+});
 
 // Metadata
 export const metadata = {
@@ -10,14 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Import Fira Sans Google Font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body style={{ fontFamily: "'Fira Sans', sans-serif" }}>
+      <body className={firaSans.className}>
         <Nav />
         {children}
       </body>
